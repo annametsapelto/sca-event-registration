56113b09af6a7ee459058924f4bf028aceeba4a7
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-07-04 17:26:34 +0300

    Add localization information in App.js

--- registration/src/App.js
+++ registration/src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
+import { FormattedMessage, useIntl, IntlProvider } from "react-intl"
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  let initLocale = "fi";
+  if (navigator.language === "en") {
+    initLocale = "en";
+  }
+
+  function loadMessages(locale) {
+    switch (locale) {
+      case "fi":
+        return import("./messages/fi.json");
+      case "en":
+        return import("./messages/en.json");
+      default :
+        return import("./messages/fi.json");
+    }
+  }
+
+  function LocalizationWrapper() {
+    const [locale, setLocale] = useState(initLocale);
+    const [messages, setMessages] = useState(null);
+
+    useEffect(() => loadMessages(locale).then(setMessages), [locale]);
+  }
   return (
     <div className="App">
 
