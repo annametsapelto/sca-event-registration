0160a2119c842d77db6ee42fb31389dc50173c7d
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-07-19 13:26:54 +0300

    Work on the languages

--- registration/src/App.js
+++ registration/src/App.js
@@ -2,12 +2,6 @@ import { FormattedMessage, useIntl, IntlProvider } from "react-intl"
 import { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  let initLocale = "fi";
-  if (navigator.language === "en") {
-    initLocale = "en";
-  }
-
   function loadMessages(locale) {
     switch (locale) {
       case "fi":
@@ -19,14 +13,37 @@ function App() {
     }
   }
 
+    let initLocale = "fi";
+  if (navigator.language === "en") {
+    initLocale = "en";
+  }
+
   function LocalizationWrapper() {
     const [locale, setLocale] = useState(initLocale);
     const [messages, setMessages] = useState(null);
 
     useEffect(() => loadMessages(locale).then(setMessages), [locale]);
+
+    return messages ? (
+      <IntlProvider locale = { locale } messages={message}>
+        <App locale={locale} onLocaleChange={(locale) => setLocale(locale)} />
+      </IntlProvider>
+    ) : null;
   }
+
+
+function App({locale, onLocaleChange}) {
+  const intl = useIntl();
   return (
     <div className="App">
+      <select value={locale} onChange={(e) => onLocaleChange(e.target.value)}>
+        <option value="fi">Finnish</option>
+        <option value="en">English</option>
+      </select>
+      <div>
+        <label for="firstName"><FormattedMessage id="messages.firstName"></FormattedMessage></label>
+        <input type="text" id="firstName"></input>
+      </div>
 
     </div>
   );
